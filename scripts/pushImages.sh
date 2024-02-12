eeba3c00375d099083295dec81194a1991e7e9ba
HoshEnder <41672932+HoshEnder@users.noreply.github.com>
2024-02-12 06:12:16 +0100

    Update pushImages.sh

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -4,6 +4,6 @@ docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-server:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
 
